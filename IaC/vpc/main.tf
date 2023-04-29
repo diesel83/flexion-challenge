ba79a0313f2b5faf39e521aa6dd7b69e7e5e9217
Rene Acevedo <acevedosr@gmail.com>
2023-04-29 09:18:43 -0500

    add firewall rules

--- IaC/vpc/main.tf
+++ IaC/vpc/main.tf
@@ -38,3 +38,36 @@ resource "google_compute_router_nat" "flexion-nat" {
     filter = "ERRORS_ONLY"
   }
 }
+
+resource "google_compute_firewall" "allow-internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_flexion.id
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+  source_ranges = ["10.2.0.0/16"]
+
+}
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_flexion.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["35.235.240.0/20"]
+}
+
+
+
